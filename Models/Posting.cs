dac49fe88efa2535c83695606be67e7d0198996d
Panzerfahrer1 <oliverangerer0@gmail.com>
2025-12-24 10:03:30 +0100

    Added all buttons to the index site

--- Models/Posting.cs
+++ Models/Posting.cs
@@ -20,40 +20,10 @@
 
         public Posting(string text, string? author = null)
         {
-            if(!CheckPost(text))
-            {
-                throw new ArgumentException("Post text cannot exceed 280 characters.");
-            }
-
             Id = _nextId++;
             Author = author;
             CreatedDate = DateTime.Now;
             Likes = 0;
         }
-
-        /// <summary>
-        /// Updates the text content with the specified value if it does not exceed 280 characters.
-        /// </summary>
-        /// <param name="newText">The new text to set. Must not be longer than 280 characters.</param>
-        /// <returns>true if the text was updated successfully; otherwise, false.</returns>
-        public bool UpdateText(string newText)
-        {
-            if(!CheckPost(newText))
-            {
-                return false;
-            }
-
-            Text = newText;
-            UpdatedDate = DateTime.Now;
-
-            return true;
-        }
-
-        /// <summary>
-        /// Determines whether the specified post text meets the maximum length requirement.
-        /// </summary>
-        /// <param name="text">The text of the post to validate. Must not exceed 280 characters.</param>
-        /// <returns>true if the post text length is 280 characters or fewer; otherwise, false.</returns>
-        public bool CheckPost(string text) => text.Length <= 280;
     }
 }
